8511339762850e4e89485fabf3c869d0c42a50c0
Sascha Beining <beining@saltation.de>
2020-05-08 23:10:41 +0200

    Implement very basic view and functionality

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Control.Monad
 import Data.Question
 import System.Directory
 import System.Environment.Executable
 import System.FilePath
+import qualified Graphics.UI.Threepenny as UI
+import Graphics.UI.Threepenny.Core
 
 main :: IO ()
 main = do
@@ -16,4 +19,29 @@ main = do
       else "questions"
 
   questions <- fromFilePath path
-  print $ questions
+
+  startGUI defaultConfig (setup questions)
+
+setup :: [Question] -> Window -> UI ()
+setup questions window = void $ do
+  _ <- return window # set UI.title "Quiz Mwxyzster"
+
+  -- GUI Elements
+  number <- UI.input
+    # set value "0"
+    # set (attr "type") "hidden"
+  question <- UI.div
+    # set text "Click to start"
+
+    -- Events
+  on UI.click question $ const $ do
+    nr <- get value number
+    let num = read nr
+    _ <- element question # set text (questionContent $ questions!!num)
+    element number # set value (show $ num + 1)
+
+  -- DOM
+  getBody window #+
+    [ element number
+    , element question
+    ]
