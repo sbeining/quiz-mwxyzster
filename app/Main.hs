15eac233c18097f5959e9f2d3854abe7421d0e41
Sascha Beining <beining@saltation.de>
2020-05-08 20:48:32 +0200

    Actually search for the „questions“ directory next to the executable
    The relative path is relative to the "current" directory not relative to
    the executable.
    The path to the executable has to be determined manually.
    To make it easier, while developing there is still a fallback to the
    directory relative to the „current“ directory

--- app/Main.hs
+++ app/Main.hs
@@ -3,9 +3,17 @@ module Main where
 
 import Data.Question
 import System.Directory
+import System.Environment.Executable
 import System.FilePath
 
 main :: IO ()
 main = do
-  questions <- fromFilePath "questions"
+  -- search in directory of the executable first and then in the current directory
+  (execPath, _) <- splitExecutablePath
+  exists <- doesDirectoryExist (execPath </> "questions")
+  let path = if exists
+      then execPath </> "questions"
+      else "questions"
+
+  questions <- fromFilePath path
   print $ questions
