0586def224c000e1ce9b1cfd13feb08039e2b500
Sascha Beining <beining@saltation.de>
2020-05-10 19:49:27 +0200

    Make „backspace“ go back one question

--- app/Main.hs
+++ app/Main.hs
@@ -27,13 +27,14 @@ setup questions window = void $ do
     # set value "0"
     # set (attr "type") "hidden"
   question <- UI.div
-    # set text "Click or press space to start"
+    # set text "Click or press space to start (Press backspace to go back.)"
 
     -- Events
   on UI.click question $ const $ nextQuestion number question
 
   body <- getBody window
   on UI.keyup body $ \keycode -> when (keycode == space) $ void $ prevQuestion number question
+  on UI.keyup body $ \keycode -> when (keycode == backspace) $ void $ nextQuestion number question
 
   -- DOM
   getBody window #+
@@ -44,6 +45,16 @@ setup questions window = void $ do
   where
     space :: Int
     space = 8
+
+    backspace :: Int
+    backspace = 32
+
+    prevQuestion :: Element -> Element-> UI Element
+    prevQuestion number outputElement = do
+      nr <- get value number
+      let num = (read nr) - 1
+      _ <- element outputElement # set text (questionContent $ questions!!(num - 1))
+      element number # set value (show num)
 
     nextQuestion :: Element -> Element-> UI Element
     nextQuestion number outputElement = do
